6bad48883ae4471298df7b6324edea241f53ff22
Kunruethai Thongjumroon <68454006+Kunruethai@users.noreply.github.com>
2020-12-22 14:55:21 +0700

    เพิ่ม case detail

---
+++ app/Http/Controllers/CaseProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Case_product;
+use Illuminate\Http\Request;
+
+class CaseProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Case_product  $case_product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Case_product $case_product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Case_product  $case_product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Case_product $case_product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Case_product  $case_product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Case_product $case_product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Case_product  $case_product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Case_product $case_product)
+    {
+        //
+    }
+}
---
+++ database/factories/CaseProductFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Case_product;
+use Faker\Generator as Faker;
+
+$factory->define(Case_product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
