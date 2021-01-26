6f238a781fc45037ff8ac733746bd5505d0e1ace
Kunruethai Thongjumroon <68454006+Kunruethai@users.noreply.github.com>
2021-01-26 10:32:15 +0700

    แก้ไขหนา login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,19 +18,25 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <div class="container-login100" style="background-image: url('assets/img/brand/bg-01.jpg');
+    min-height:100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-gray shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand navbar-light " href="{{ url('/') }}">
+
                     Free Product Trakool
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon "></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse " id="navbarSupportedContent " >
                     <!-- Left Side Of Navbar -->
 
 
