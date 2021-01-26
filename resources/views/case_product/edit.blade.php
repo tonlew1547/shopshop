9d929dfca15a39fde5d36e6b18225fcbb55d7065
BOBLEE <76192084+tonlew1547@users.noreply.github.com>
2021-01-26 14:19:41 +0700

    ย้อนกลับ edit case_product

--- resources/views/case_product/edit.blade.php
+++ resources/views/case_product/edit.blade.php
@@ -97,14 +97,16 @@
 
 
 
-                    <div class="row mt-2 col-2">
-                        <div class="col">
-                            
-                            {!! Form::submit('บันทึก', ['class' => 'btn btn-primary']) !!}
+                 <div class="row mt-2">
+                    <div class="col">
 
-
-
+                        {!! Form::submit('บันทึก', ['class' => 'btn btn-primary']) !!}
+                        <a href="{{url('case_product')}}" class="btn btn-primary" role="button">ย้อนกลับ</a>
                     </div>
+                </div>
+
+
+                  
                     {!! Form::close() !!}
             </div>
         </div>
