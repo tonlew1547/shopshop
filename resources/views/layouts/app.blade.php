c4c4a5c0533498fa0ca92b32ba289e7071e0605f
Kunruethai Thongjumroon <68454006+Kunruethai@users.noreply.github.com>
2021-01-26 10:47:36 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
             <div class="container">
                 <a class="navbar-brand navbar-light " href="{{ url('/') }}">
 
-                    Free Product Trakool
+                   Stock Product Trakool
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon "></span>
